787c3779d4b79672ae69f26cc9c2455406e7a289
cholong <sentilemon02@gmail.com>
2022-07-15 00:05:53 +0900

    update thread code

--- thread_socket/client.py
+++ thread_socket/client.py
@@ -7,14 +7,22 @@ PORT = 9999
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
+def recv_data(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        
+        print(f'Receive: {repr(data.decode())}')
+        
+        
+start_new_thread(recv_data, (client_socket,))
+
+
 while True:
     message = input('Enter Message: ')
     if message == 'quit':
         break
     
     client_socket.send(message.encode())
-    data = client_socket.recv(1024)
     
-    print(f'Received form the server: {repr(data.decode())}')
     
 client_socket.close()
--- thread_socket/server.py
+++ thread_socket/server.py
@@ -4,45 +4,58 @@ from _thread import *
 
 # 접속한 클라이언트마다 새로운 thread 생성
 def thread_socket(client_socket, addr):
-    print(f'Connected by: {addr[0]}, :, {addr[1]}')
+    print(f'Connected by: {addr[0]}, {addr[1]}')
     
     # 클라이언트 연결 해제까지 반복
     while True:
         
         try:
+            # 클라이언트 -> 데이터 수신 -> 클라이언트 다시 전송(에코)
             data = client_socket.recv(1024)
             
             if not data:
-                print(f'Disconnected by: {addr[0]}, :, {addr[1]}')
+                print(f'Disconnected by: {addr[0]}, {addr[1]}')
                 break
             
-            print(f'Received from: {addr[0]}, :, {addr[1]}, {data.decode()}')
+            print(f'Received from: {addr[0]}, {addr[1]}, {data.decode()}')
             
-            client_socket.send(data)
+            for client in client_sockets:
+                if client != client_socket:
+                    client.send(data)
+                    
         except ConnectionResetError as e:
-            print(f'Disconnected by: {addr[0]}, :, {addr[1]}')
+            print(f'Disconnected by: {addr[0]}, {addr[1]}')
             break
         
+    if client_socket in client_sockets:
+        client_sockets.remove(client_socket)
+        print(f'Remove 참가 클라이언트 목록: {len(client_sockets)}')
     client_socket.close()
     
 
+client_sockets = []  # 서버에 접속할 클라이언트 목록
 HOST = '127.0.0.1'
 PORT = 9999
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((HOST, PORT))
+server_socket.bind(('', PORT))
 server_socket.listen()
 
 print('::::::::::::::::::')
 print(':: SERVER START ::')
 print('::::::::::::::::::')
 
-while True:
-    print('::     WAIT     ::')
-    print('::::::::::::::::::')
-    
-    client_socket, addr = server_socket.accept()
-    start_new_thread(thread_socket, (client_socket, addr))
-
-server_socket.close()
+try:
+    while True:
+        print('::     WAIT     ::')
+        print('::::::::::::::::::')
+        
+        client_socket, addr = server_socket.accept()
+        client_sockets.append(client_socket)
+        start_new_thread(thread_socket, (client_socket, addr))
+        print(f'참가 클라이언트 목록: ', len(client_sockets))
+except Exception as e:
+    print(f'Error: {e}')        
+finally:
+    server_socket.close()
